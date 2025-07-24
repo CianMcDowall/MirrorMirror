77c0273c788f815e01238049996c95661451df44
CianMcDowall <cianmcdowall1@gmail.com>
2025-07-24 14:12:52 +1000

    Added News Articles

--- src/Interpreter.java
+++ src/Interpreter.java
@@ -6,14 +6,31 @@ public class Interpreter {
         if(location.trim().equalsIgnoreCase("Home"))
         {
             locationNumber = 0;
-        } else if(location.equalsIgnoreCase("University") || location.equalsIgnoreCase("Uni"))
+        } else if(location.trim().equalsIgnoreCase("University") || location.trim().equalsIgnoreCase("Uni"))
         {
             locationNumber = 1;
-        } else if(location.equalsIgnoreCase("Work"))
+        } else if(location.trim().equalsIgnoreCase("Work"))
         {
             locationNumber = 2;
+        } else if(location.trim().equalsIgnoreCase("All"))
+        {
+            return new Weather();
         }
 
         return new Weather(locationNumber);
+    }
+
+    public static Article getNews(String section) throws Exception
+    {
+        int sectionNumber = -1;
+        if(section.trim().equalsIgnoreCase("Aus") || section.trim().equalsIgnoreCase("Australia"))
+        {
+            sectionNumber = 0;
+        } else if(section.trim().equalsIgnoreCase("World"))
+        {
+            sectionNumber = 1;
+        }
+
+        return new Article(sectionNumber);
     }
 }
